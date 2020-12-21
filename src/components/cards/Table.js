53fc09b7ea86c18df7003101bafc433ff969b81c
Garza0 <konovalovandrey7@gmail.com>
2020-12-21 20:54:56 +0200

    feat: card table styles

--- src/components/cards/Table.js
+++ src/components/cards/Table.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { actionCreator } from '../../store/actions';
+import './Table.css';
 
 function Table() {
   const dispatch = useDispatch();
